0673ba44e902bf81aea9b3d2131ae4305cc4a7d8
Ivor Staats <ivor.staats@student.aventus.nl>
2017-01-30 15:38:16 +0100

    nav

--- nav.php
+++ nav.php
@@ -13,9 +13,6 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-<!-- //////////// -->
-
-
                     <li>
                         <a href="info.php">Info</a>
                     </li>
@@ -37,27 +34,11 @@
                     <li>
                         <a href="register.php">Registreren</a>
                     </li>
-<!-- //////////// -->
-
-
-<!-- //////////// -->
-
-                <li>
-                    <a href="info.php">Info</a>
-                </li>
-                <li>
-                    <a href="cursus_overzicht.php">Cursussen</a>
-                </li>
-                <li>
-                    <a href="contact.php">Contact</a>
-                </li>
-                <li>
-                    <a href="logout.php">Logout</a>
-                </li>
-<!-- //////////// -->
-
-<!-- //////////// -->
-
+                    <li>
+                        <?php if(isset($_SESSION['login'])){
+                            echo "<a href='logout.php'>Logout</a>";
+                        } ?>
+                    </li>
             </ul>
         </div>
         <!-- /.navbar-collapse -->
